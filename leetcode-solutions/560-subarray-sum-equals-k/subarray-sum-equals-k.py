5baec355b4e9ca775f9f73f948a2fdcd621b27fb
Natnael Endale Cheru <natnaelendale22@gmail.com>
2026-02-03 13:43:43 +0300

    Time: 28 ms (76.14%) | Memory: 22 MB (21.89%) - LeetSync

---
+++ leetcode-solutions/560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0
+        hash_map = {0:1}
+        prefix_sum = 0
+
+        for num in nums:
+            prefix_sum += num
+            if prefix_sum - k in hash_map:
+                count += hash_map[prefix_sum - k]
+
+            hash_map[prefix_sum] = hash_map.get(prefix_sum, 0) + 1
+
+        return count
+        
