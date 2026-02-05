2db542fb73efe165d93940e48b2434c981dccb51
Natnael Endale Cheru <natnaelendale22@gmail.com>
2026-02-05 10:20:51 +0300

    Time: 99 ms (55.28%) | Memory: 34.4 MB (16.61%) - LeetSync

---
+++ leetcode-solutions/739-daily-temperatures/daily-temperatures.py
@@ -0,0 +1,14 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        result = [0] * n
+        stack = [] # stack consists of a tuple the temperature and it's index
+
+        for i, t in enumerate(temperatures):
+            while stack and stack[-1][0] < t:
+                stack_t, stack_i = stack.pop()
+                result[stack_i] = i - stack_i
+
+            stack.append((t, i))
+
+        return result
